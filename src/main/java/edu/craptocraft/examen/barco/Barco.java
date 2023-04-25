04cedf241d4fa1c87db2c5da91f4de1d9fe64762
ncocana <ncocana@cifpfbmoll.eu>
2023-04-25 16:58:23 +0200

    feat(Barco): add mayorSize() method

---
+++ src/main/java/edu/craptocraft/examen/barco/Barco.java
@@ -0,0 +1,50 @@
+package edu.craptocraft.examen.barco;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class Barco {
+    
+    private String matricula = "";
+    private String nombre = "";
+    private float size = 0;
+
+    public Barco(String matricula, String nombre, float size) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.size = size;
+    }
+
+    public String getMatricula() {
+        return this.matricula;
+    }
+
+    private void setMatricula(String newMatricula) {
+        this.matricula = newMatricula;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String newNombre) {
+        this.nombre = newNombre;
+    }
+
+    public float getSize() {
+        return this.size;
+    }
+
+    private void setSize(float newSize) {
+        this.size = newSize;
+    }
+
+    public static String mayorSize(List<Barco> barcosEntrantes) {
+        Optional<Barco> maxSize = barcosEntrantes.stream().max(Comparator.comparing(Barco::getSize));
+        return maxSize.map(Barco::getMatricula).orElse(null);
+    }
+
+}
